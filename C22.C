dc670792162814038df2238287926b18b424ab83
SAGAR T <ssagu6661@gmail.com>
2024-08-21 13:40:21 -0400

    Add files via upload

---
+++ C22.C
@@ -0,0 +1,43 @@
+                           /*  REQUISTICS  FUNCTIONS*/
+/*ODU*//*ondu sala kintha hechu bariyuvudu ke function hanu use maduthare*/
+    
+    
+    #include<stdio.h>
+    int sumtwo(int a,int b);
+    int main(){
+        int a= 4550, b=2550,c;
+        c=sumtwo(a,b);
+        printf("%d", c);
+        return 0;
+    }
+int sumtwo(int a,int b){
+    return a + b;
+} 
+
+
+// #include<stdio.h>
+// void sumtwo(int a, int b);
+// int main(){
+//     int a=10,b= 20,c;
+//     sumtwo(a,b);
+//     return 0;
+      
+// }
+// void sumtwo(int a, int b){
+//     printf("addition of the to number = %d",a-b);
+// }
+
+
+
+
+/*#include<stdio.h>
+void sumtwo(int a, int b);
+int main(){
+    int a=50,b= 20,c;
+    sumtwo(a,b);
+    return 0;
+      
+}
+void sumtwo(int a, int b){
+    printf("subtraction of the to number = %d",a-b);
+}*/
